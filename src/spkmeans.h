5d948d6e5a3f9259e01192e5b1f0819a73acc531
yara <80980659+yarasalami@users.noreply.github.com>
2022-08-21 23:36:36 +0300

    Update spkmeans.h
    1. the enum should always be in the header file
    2. I added a header for two of the function: calculateWeight and getWeightedMatrix
    3. The #ifdef directive is useful for checking whether a definition exists

--- src/spkmeans.h
+++ src/spkmeans.h
@@ -1,3 +1,36 @@
-#define EPSILON pow(10, -5)
-#define LINESIZE 1000
-double** createMat(int, int);
+#ifndef spkmeans_h
+#define spkmeans_h
+
+typedef enum {SPK,WAM, DDG, LNORM, JACOBI
+} Goal;
+
+/**
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc,char **argv);
+
+/**
+ *
+ * @param matrix
+ * @param n
+ * @return
+ * this func Form the weighted adjacency matrix W ∈ R^(n×n),
+ * for the matrix given as input and returns it.
+ */
+double** getWeightedMatrix(double** matrix, int n);
+
+/**
+ *
+ * @param matrix
+ * @param index1
+ * @param index2
+ * @param dim
+ * @return
+ * this function calculate the value of exp(−(||xi − xj||/2)), and returns it.
+ */
+double calculateWeight(double** matrix, int index1, int index2, int dim);
+
+#endif
