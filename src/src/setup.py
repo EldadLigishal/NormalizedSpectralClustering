d8d77d886484ce21749c49854ea137107d06531e
EldadLigishal <80391325+EldadLigishal@users.noreply.github.com>
2022-08-24 14:52:48 +0300

    Delete src/src directory

--- src/src/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(
-    name='mySpkmeans',
-    author='Yara and Eldad',
-    version='1.0',
-    ext_modules=[Extension('mySpkmeans',sources=['spkmeans.c', 'spkmeansmodule.c'])])
