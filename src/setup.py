0e0cc20f62687194ede4ad9e965a7164c7de356a
yara <80980659+yarasalami@users.noreply.github.com>
2022-08-25 15:03:48 +0300

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,4 +4,5 @@ setup(
     name='mySpkmeans',
     author='Yara and Eldad',
     version='1.0',
+    description='kmeans final',
     ext_modules=[Extension('mySpkmeans',sources=['spkmeans.c', 'spkmeansmodule.c'])])
