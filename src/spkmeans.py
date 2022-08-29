d3b7e9e8815342b8c23d1efeacb896fd3857c223
yara salami <80980659+yarasalami@users.noreply.github.com>
2022-08-29 17:48:03 +0300

    Update spkmeans.py

--- src/spkmeans.py
+++ src/spkmeans.py
@@ -4,7 +4,9 @@ import numpy as np
 import pandas as pd
 import sys
 import mySpkmeans as km
-EPSILON = 10**-10   # good epsilon?
+
+EPSILON = 10 ** -10
+MAXITR = 100
 
 
 class Goal(enum):
@@ -14,56 +16,50 @@ class Goal(enum):
     LNORM = "lnorm"
     JACOBI = "jacobi"
 
-    def has_value(self, value):
-        return (value == self.SPK) or (value == self.WAM) or (
-            value == self.DDG) or (self.LNORM) or (self.JACOBI)
 
+# check if a value exists in an enum:
+def has_value(self, value):
+    return (value == self.SPK) or (value == self.WAM) or \
+           (value == self.DDG) or (value == self.LNORM) or (value == self.JACOBI)
 
 
 # k := the number of clusters required.
-def execute(k, goal,input_filename):
-
-    #reading input file
+def execute(k, goal, input_filename):
+    # reading input file
     try:
-        input_matrix = pd.read_csv(input_filename)
+        input_matrix = pd.read_csv(input_filename, header=None)
     except:
         print("An Error Has Occurred")
         return 0
 
-    
     # check if goal is valid
-    if not Goal.has_value(goal):
+    if not has_value(Goal, goal):
         print("Invalid Input!")
         return 0
 
     matrix = input_matrix.to_numpy()
-    # n := number of rows of the input file.
-    # d := number of columns of the input file.
-    n = matrix.shape(0)
-    d = matrix.shape(1)
+    # n := number of line/rows of the input file = number of vectors = len(inputMat).
+    n = len(matrix)
+    # d := number of column of an input file.
+    d = len(matrix[0])
 
     # Check if the data is correct
+    # k must be < the number of datapoints in the file given
     if (k >= n) or (k < 0):
         print("Invalid Input!")
         return 0
 
-
-    if (goal == Goal.DDG) or (
-        goal == Goal.WAM) or (
-        goal == Goal.JACOBI) or (
-            goal == Goal.LNORM):
-        km.fit(k, n, d, goal, matrix)
-    
-    elif goal == Goal.SPK:
+    if goal == Goal.SPK:
         # centroids Âµ1, Âµ2, ... , ÂµK âˆˆ R^d where 1<K<N.
+        Tmatrix = km.fitgetTmat(matrix,k,d,n)
+        Tmatrix = np.array((Tmatrix))
+        k = len(Tmatrix[0])
         centroids = buildCentroids(k, n, matrix)
-        spk_matrix = km.fit_spk(k, 100, EPSILON, n, d, matrix, centroids.tolist())
+        spk_matrix = km.fit(k, MAXITR, EPSILON, n, d, Tmatrix, centroids.tolist())
         printMatrix(np.array(spk_matrix))
-
     else:
-        print("Invalid Input!")
-        return 0
-
+        # goal == Goal.DDG or Goal.WAM or Goal.JACOBI or Goal.LNORM
+        km.fit(k, n, d, goal, matrix)
 
 
 def buildCentroids(k, n, input_matrix):
@@ -86,7 +82,7 @@ def buildCentroids(k, n, input_matrix):
         prob = np.zeros(n)
         sum_matrix = np.sum(d)
         for j in range(n):
-            prob[j] = d[j]/sum_matrix
+            prob[j] = d[j] / sum_matrix
 
         rand_i = np.random.choice(n, p=prob)
         centroids_index[i] = rand_i
@@ -97,7 +93,7 @@ def buildCentroids(k, n, input_matrix):
     # as the initial centroids. Observationâ€™s index is given by the first column in each input file.
     printIndex(centroids_index)
     return centroids
- 
+
 
 def step1(i, vector, matrix):
     min_vector = math.pow(np.linalg.norm(np.subtract(vector, matrix[0])), 2)
@@ -136,5 +132,4 @@ if input_argc == 4:
     # (k, goal, inputFile1)
     execute(int(input_argv[1]), input_argv[2], input_argv[3])
 else:
-    # (k, epsilon, inputFile1, inputFile2)
     print("Invalid Input!")
