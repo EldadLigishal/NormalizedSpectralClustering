51a54ac91cc664084c0274a9118bab7fe8734fa0
yara salami <80980659+yarasalami@users.noreply.github.com>
2022-09-08 15:06:47 +0300

    last update :)
    I added a description.

--- src/setup.py
+++ src/setup.py
@@ -4,4 +4,5 @@ setup(
     name='myspkmeans',
     author='Yara and Eldad',
     version='1.0',
-    ext_modules=[Extension('myspkmeans',sources=['spkmeans.c', 'spkmeansmodule.c'])])
+    description='Python wrapper for custom C extension',
+    ext_modules=[Extension('myspkmeans', sources=['spkmeans.c', 'spkmeansmodule.c'])])
